d76c95f726c0a9feb6bce7fbfd633cbbc123ffa7
github username <github email address>
2019-08-15 19:31:13 -0400

    Flesh out nonogram controller

--- app/controllers/nonograms_controller.rb
+++ app/controllers/nonograms_controller.rb
@@ -1,2 +1,21 @@
 class NonogramsController < ApplicationController
+
+  def create
+    @nonogram=Nonogram.create(nonogram_params)
+    render json: @nonogram, status: 201
+  end
+
+  def show
+    @nonogram=Nonogram.find_by(id: params[:id])
+    render json: @nonogram
+  end
+
+
+  private
+
+  def nonogram_params
+    params.require(:nonogram).permit(:solution, :user_id, :length, :width)
+  end
+
+
 end
