c0f99e5e3c93e5ea3e78326eaf3c4fa60a8f935f
github username <github email address>
2019-08-19 23:21:56 -0400

    Add clues to grid rendering

--- client/src/components/nons/Grid.js
+++ client/src/components/nons/Grid.js
@@ -9,19 +9,36 @@ class Grid extends Component {
   mapGrid=()=>{
 
     const grid = this.props.grid.map((row, y)=>{
-
       return (<tr key={y}>{this.mapRow(row, y)}</tr>)
     })
+
+    grid.unshift(this.mapColClues())
 
     return grid;
   }
 
-  mapRow(row, y){
-    return(
-      row.map((cell, x)=>{
-        return (<td style={cell===1 ? {backgroundColor: 'black'} : {backgroundColor: 'white'}} key={x.toString()+','+y.toString()} id={x.toString()+','+y.toString()} onClick={this.props.handleOnClick}>{}</td>)
-      })
+  mapColClues(){
+
+    let colClues=this.props.colClues.map(clueGroup=>
+      <th>{clueGroup.join('\n')}</th>
     )
+
+    colClues.unshift(<td></td>)
+
+    return colClues
+  }
+
+  mapRow(row, y){
+    let mappedRow = row.map((cell, x)=>{
+        return (<td style={cell===1 ? {backgroundColor: 'black'} : {backgroundColor: 'white'}} key={x.toString()+','+y.toString()} id={x.toString()+','+y.toString()} onClick={this.props.handleOnClick}>{}</td>)
+    })
+
+    mappedRow.unshift(this.getRowClue(y))
+    return mappedRow
+  }
+
+  getRowClue(rowNum){
+    return (<th>{this.props.rowClues[rowNum].join(' ')}</th>)
   }
 
   render() {
