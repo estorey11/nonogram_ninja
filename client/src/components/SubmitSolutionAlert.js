d3c664906ecd926b1018480875c6ccc1b88afa65
github username <github email address>
2019-08-21 11:56:29 -0400

    Add buttons to submission alert

--- client/src/components/SubmitSolutionAlert.js
+++ client/src/components/SubmitSolutionAlert.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import Alert from 'react-bootstrap/Alert'
+import Button from 'react-bootstrap/Button'
 
 const SubmitSolutionAlert = props =>
   <Alert variant={props.variant} show={props.show}>
-    {props.text} Placeholder text!
+    {props.text}
+    <div>
+      <Button
+        show={props.buttonShow}
+        style={{margin: '10px'}}
+        onClick={props.onClick}
+        variant="outline-danger">
+              Try Again
+      </Button>
+      <Button
+        show={props.buttonShow}
+        style={{margin: '10px'}}
+        onClick={props.onClick}
+        variant="outline-danger">
+              See Solution
+      </Button>
+    </div>
   </Alert>
 
 export default SubmitSolutionAlert;
